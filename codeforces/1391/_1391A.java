e91c9b601486358753dfb414a896eb48e47c4e6b
Tarang <tp0265@gmail.com>
2020-08-12 11:57:33 +0530

    Codeforces 1391 Div 2

---
+++ codeforces/1391/_1391A.java
@@ -0,0 +1,68 @@
+import java.io.*;
+
+public class _1391A {
+    BufferedReader bf;
+    PrintWriter writer;
+    StringBuilder sb;
+    static boolean local_system = false;
+
+    void run() throws IOException {
+        int t = i();
+        while(t-->0){
+            int n = i();
+            int[] d = new int[n];
+            for(int i = 0,h=n;i<n;i++,h--)
+                d[i] = h;
+            for(int e : d){
+                sb.append(e).append(" ");
+            }
+            sb.append("\n");
+        }
+        writer.println(sb.toString().trim());
+    }
+
+    public static void main(String[] args) throws IOException {
+        long start_time = System.currentTimeMillis();
+        _1391A obj = new _1391A();
+        obj.run();
+        long end_time = System.currentTimeMillis();
+        if (local_system) obj.writer.println("Time : " + (end_time - start_time));
+        obj.close();
+    }
+
+    public _1391A(){
+        writer = new PrintWriter(System.out);
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+    }
+
+    public int i() throws IOException {
+        return Integer.parseInt(bf.readLine());
+    }
+
+    public long l() throws IOException {
+        return Long.parseLong(bf.readLine());
+    }
+
+    public int[] ni() throws IOException {
+        String[] data = bf.readLine().split(" ");
+        int[] send = new int[data.length];
+        for (int i = 0, h = data.length; i < h; i++) send[i] = Integer.parseInt(data[i]);
+        return send;
+    }
+
+    public long[] nl() throws IOException {
+        String[] data = bf.readLine().split(" ");
+        long[] send = new long[data.length];
+        for (int i = 0, h = data.length; i < h; i++) send[i] = Long.parseLong(data[i]);
+        return send;
+    }
+
+    public void close() throws IOException{
+        writer.flush();
+        writer.close();
+        bf.close();
+    }
+}
+
+
